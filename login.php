a14d3e65a52b4c94703e834dce5749c45bf74314
krishna799 <kk.sv11@gmail.com>
2017-05-03 10:06:18 +0530

    Create login.php
    added new file

---
+++ login.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+<title></title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+</head>
+<body background="kalyan.jpg" style="width:1366px;height:768px;">
+<marquee><img src="mm.jpeg" alter="movie magic "width="60" height="60"><font size="10" color="white">MOVIE MAGIC</font></marquee>
+
+<form action="NewServlet" method="get">
+<table align="center">
+<tr>
+<td><b>USERNAME:</b></td><td><input type="text" name="username"></td>
+</tr><a href="chaits.html"></a>
+<br>
+<tr>
+<td><b>PASSWORD:</b></td><td><input type="password" name="password"></td>
+</tr>
+<br>
+<tr>
+
+<td padding-right:150px>
+
+<pre><input type="submit" value="submit"></pre>
+</td>
+</tr>
+</table>
+
+
+</body>
+</html>
